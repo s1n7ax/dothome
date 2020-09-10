f2441e2c532f3b887a7cec7d3ea8fff7c5b5ed73
s1n7ax <srineshnisala@gmail.com>
2020-09-10 08:45:29 +0530

    [ADD] add flatpak bin and local bin to PATH

--- .local/share/s1n7ax/init/env.sh
+++ .local/share/s1n7ax/init/env.sh
@@ -3,6 +3,8 @@
 # this file contains all the environment variables
 export PATH=$PATH:~/.local/bin
 export PATH=$PATH:~/.yarn/bin
+export PATH=$PATH:~/.local/share/flatpak/exports/bin
+export PATH=$PATH:/var/lib/flatpak/exports/bin
 
 export EDITOR=nvim
 
