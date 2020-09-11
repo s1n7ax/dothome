689634005a45732f7572063725b5bc6e3b247977
s1n7ax <srineshnisala@gmail.com>
2020-09-11 08:00:52 +0530

    [UPD] changed alternative editor to nano

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -49,7 +49,7 @@ beautiful.init(gears.filesystem.get_themes_dir() .. "xresources/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "st"
-editor = os.getenv("EDITOR") or "nvim"
+editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 
 -- Default modkey.
