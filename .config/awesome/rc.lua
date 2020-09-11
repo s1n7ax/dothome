0454d4882126ee04e0a3986c1e2f8c3b7ea0da06
s1n7ax <srineshnisala@gmail.com>
2020-09-11 08:24:10 +0530

    [UPD] i have no idea why I commented them in the first place

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -457,17 +457,17 @@ for i = 1, 9 do
 end
 
 clientbuttons = gears.table.join(
-    awful.button({ }, 1, function (c)
-        c:emit_signal("request::activate", "mouse_click", {raise = true})
-    end),
-    awful.button({ modkey }, 1, function (c)
-        c:emit_signal("request::activate", "mouse_click", {raise = true})
-        awful.mouse.client.move(c)
-    end),
-    awful.button({ modkey }, 3, function (c)
-        c:emit_signal("request::activate", "mouse_click", {raise = true})
-        awful.mouse.client.resize(c)
-    end)
+	awful.button({ }, 1, function (c)
+		c:emit_signal("request::activate", "mouse_click", {raise = true})
+	end),
+	awful.button({ modkey }, 1, function (c)
+		c:emit_signal("request::activate", "mouse_click", {raise = true})
+		awful.mouse.client.move(c)
+	end),
+	awful.button({ modkey }, 3, function (c)
+		c:emit_signal("request::activate", "mouse_click", {raise = true})
+		awful.mouse.client.resize(c)
+	end)
 )
 
 -- Set keys
