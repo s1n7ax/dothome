254ccc80aa6804300f05a9a11fcc35a1eff407ff
s1n7ax <srineshnisala@gmail.com>
2020-10-25 08:46:26 +0530

    [UPD] move custom exe from .local/bin to .local/share/s1n7ax/bin

--- .local/share/s1n7ax/init/env.sh
+++ .local/share/s1n7ax/init/env.sh
@@ -2,6 +2,7 @@
 
 # this file contains all the environment variables
 export PATH=$PATH:~/.local/bin
+export PATH=$PATH:~/.local/share/s1n7ax/bin
 export PATH=$PATH:~/.yarn/bin
 export PATH=$PATH:~/.local/share/flatpak/exports/bin
 export PATH=$PATH:/var/lib/flatpak/exports/bin
