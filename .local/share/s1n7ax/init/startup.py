b7fa3c64b4fa54ab8c94306d4c0dbdb7ff6ba525
s1n7ax <srineshnisala@gmail.com>
2020-10-25 08:04:34 +0530

    [UPD] change composite manager from picom to xcompmrg

--- .local/share/s1n7ax/init/startup.py
+++ .local/share/s1n7ax/init/startup.py
@@ -8,6 +8,7 @@ user = os.environ['USER']
 
 sys.path.append(os.path.abspath("{}/.local/share/s1n7ax/commons".format(home)))
 
+# execute needs to be imported after adding the module to path
 from execute import execute
 
 execute([['xsetroot', '-name', ' {} '.format(user)]], False)
@@ -20,8 +21,8 @@ execute([['chromium']], False)
 execute([['imwheel', '-b', '45']], False)
 execute([['setxkbmap', '-option', 'ctrl:nocaps']], False)
 execute([['xset', 'r', 'rate', '180', '80']], False)
-execute([['picom']], False)
-# execute([['xcompmgr']], False)
+#  execute([['picom']], False)
+execute([['xcompmgr']], False)
 execute([['wal', '-R']], False)
 execute([['dwm-status']], False)
 execute([['sxhkd']], False)
