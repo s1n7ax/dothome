d9994e1c63a70262552ba6ef316c5ec34dad4fdf
s1n7ax <srineshnisala@gmail.com>
2021-12-25 17:21:26 +0530

    chore: path env updates

--- .s1n7ax/init/global-env.sh
+++ .s1n7ax/init/global-env.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-# this file contains all the environment variables
-export PATH=$PATH:/var/lib/flatpak/exports/bin
-export PATH=$PATH:~/.s1n7ax/bin
-export PATH=$PATH:~/.yarn/bin
-export PATH=$PATH:~/.local/bin
-export PATH=$PATH:~/.cargo/bin
+PATH=$PATH:/var/lib/flatpak/exports/bin
+PATH=$PATH:~/.s1n7ax/bin
+PATH=$PATH:~/.yarn/bin
+PATH=$PATH:~/.local/bin
+PATH=$PATH:~/.cargo/bin
+export PATH
 
 export EDITOR=nvim
 export TERM=st
 
+# need this for idea IDE to work correctly on dwm
 export _JAVA_AWT_WM_NONREPARENTING=1
-
-eval $(luarocks path)
-
