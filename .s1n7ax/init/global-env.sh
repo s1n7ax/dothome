eeeeb0581752c972d0b2fbd396c6ec5fb9a0e7c5
s1n7ax <srineshnisala@gmail.com>
2022-01-07 09:24:59 +0530

    feat: update env variables

--- .s1n7ax/init/global-env.sh
+++ .s1n7ax/init/global-env.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-PATH=$PATH:/var/lib/flatpak/exports/bin
-PATH=$PATH:~/.s1n7ax/bin
-PATH=$PATH:~/.yarn/bin
-PATH=$PATH:~/.local/bin
-PATH=$PATH:~/.cargo/bin
-export PATH
+export PATH=$PATH:/var/lib/flatpak/exports/bin
+export PATH=$PATH:~/.s1n7ax/bin
+export PATH=$PATH:~/.yarn/bin
+export PATH=$PATH:~/.local/bin
+export PATH=$PATH:~/.cargo/bin
 
 export EDITOR=nvim
 export TERM=st
