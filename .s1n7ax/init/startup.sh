176640c2bf1996ef3389927ef062b9ab2e84bc90
s1n7ax <srineshnisala@gmail.com>
2022-08-23 07:39:29 +0530

    refactor: change startup applications

--- .s1n7ax/init/startup.sh
+++ .s1n7ax/init/startup.sh
@@ -7,8 +7,8 @@ nautilus || dolphin || thunar &
 pavucontrol &
 /usr/lib/kdeconnectd &
 
-flatpak run com.spotify.Client &
-flatpak run com.valvesoftware.Steam &
+# flatpak run com.spotify.Client &
+# flatpak run com.valvesoftware.Steam &
 
 xsetroot -name "  $(whoami)  "
 
