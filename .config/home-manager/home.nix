9651400b390af3223390a3347440978f0ac0e0dc
s1n7ax <srineshnisala@gmail.com>
2023-08-23 11:54:53 +0530

    chore: code clean up

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -64,28 +64,9 @@ in
     slurp
     xdg-utils
 
-    (pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; })
-
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+    (nerdfonts.override { fonts = [ "FiraCode" ]; })
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
     ".config/nvim".source = pkgs.fetchgit {
       url = "https://github.com/AstroNvim/AstroNvim";
@@ -112,18 +93,6 @@ in
       sha256 = "xa9ptHLowpz7MecbunzrgWaOstto5lsA3it9JJzK+70=";
       leaveDotGit = true;
     };
-
-
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
   };
 
   # You can also manage environment variables but you will have to manually
