374c9e42665d7219583157c4efefa38fea83cffd
s1n7ax <srineshnisala@gmail.com>
2023-08-24 15:07:27 +0530

    fix: hyprland crashes on monitor unplug
    The latest version of hyprland is crashing when the monitor cable is unplugged.
    To resolve the issue, 23.05 hyprland package is used instead of unstable one

--- .config/home-manager/flake.nix
+++ .config/home-manager/flake.nix
@@ -4,25 +4,31 @@
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.05";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = { nixpkgs, nixpkgs-stable, home-manager, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+      pkgs-stable = nixpkgs-stable.legacyPackages.${system};
     in {
       homeConfigurations."s1n7ax" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
+        extraSpecialArgs = {
+          inherit pkgs-stable;
+        };
 
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
         modules = [
           ./home.nix
           ./scripts.nix
+          ./hyprland.nix
           ./programs/alacritty
           ./programs/nushell
         ];
