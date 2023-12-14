98cc021e3453df2c9335060cea873e837c1d1de1
s1n7ax <srineshnisala@gmail.com>
2023-12-14 11:39:10 +0530

    feat: nix channel update

--- .config/home-manager/flake.nix
+++ .config/home-manager/flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.11";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
