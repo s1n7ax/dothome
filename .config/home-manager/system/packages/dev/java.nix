2c9013b53568af15d1dc7c2835c8e309c357b848
s1n7ax <srineshnisala@gmail.com>
2024-07-13 15:20:34 +0530

    chore: pkg update

--- .config/home-manager/system/packages/dev/java.nix
+++ .config/home-manager/system/packages/dev/java.nix
@@ -1,7 +1,4 @@
-{ pkgs, ... }: {
-  home.packages = with pkgs;
-    [
-
-      jdk17
-    ];
+{ pkgs, ... }:
+{
+  home.packages = with pkgs; [ jdk17 ];
 }
--- .config/home-manager/system/packages/utility.nix
+++ .config/home-manager/system/packages/utility.nix
@@ -24,6 +24,7 @@
 
     # browsers
     firefox
+    ladybird
 
     #bluetooth
     blueman
