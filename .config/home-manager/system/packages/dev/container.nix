b95f02c50d2a4945f9b443acfb09bee534f4176e
s1n7ax <srineshnisala@gmail.com>
2024-08-02 11:17:11 +0530

    feat: add docker ls

--- .config/home-manager/system/packages/dev/container.nix
+++ .config/home-manager/system/packages/dev/container.nix
@@ -1,10 +1,16 @@
-{ pkgs, pkgs-devenv, pkgs-my, ... }: {
+{
+  pkgs,
+  pkgs-devenv,
+  pkgs-my,
+  ...
+}:
+{
 
   home.packages = with pkgs; [
     pkgs-devenv.devenv
     pkgs-my.devcontainer
     lazydocker
-
+    dockerfile-language-server-nodejs
     hadolint
   ];
 }
